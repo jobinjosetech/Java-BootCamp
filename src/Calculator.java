e91fc1f19a6ee5714db9890995a94544f0a744f5
jobin <jobinjose.in@gmail.com>
2022-11-21 10:59:24 +0530

    looping

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,39 @@
 import java.util.Scanner;
 
 public class Calculator {
-    static void addition(int num1, int num2) {
+    static void addition() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
         System.out.println("Sum of "+num1+" and "+num2+" = "+(num1+num2));
     }
 
-    static void subraction(int num1, int num2) {
+    static void subraction() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
         System.out.println("Subtraction of "+num1+" and "+num2+" = "+(num1-num2));
     }
 
-    static void multiplication(int num1, int num2) {
+    static void multiplication() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
         System.out.println("product of "+num1+" and "+num2+" = "+(num1*num2));
     }
 
-    static void division(int num1, int num2) {
+    static void division() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
         float value = (float) num1/num2;
         System.out.println(String.format("Division of %d and %d = %.1f", num1, num2, value));
 
@@ -21,33 +41,32 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("------------------ Menu -----------------");
-        System.out.println("1 -> Addition");
-        System.out.println("2 -> Subtraction");
-        System.out.println("3 -> Multiplication");
-        System.out.println("4 -> Division");
-        System.out.println("-----------------------------------------");
-        System.out.print("Please select an operation: ");
-        int operation = sc.nextInt();
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
+        while(true) {
+            System.out.println("------------------ Menu -----------------");
+            System.out.println("1 -> Addition");
+            System.out.println("2 -> Subtraction");
+            System.out.println("3 -> Multiplication");
+            System.out.println("4 -> Division");
+            System.out.println("-----------------------------------------");
+            System.out.print("Please select an operation: ");
+            int operation = sc.nextInt();
 
-        switch (operation)
-        {
-            case(1):
-                addition(num1, num2);
-                break;
-            case(2):
-                subraction(num1, num2);
-                break;
-            case(3):
-                multiplication(num1, num2);
-                break;
-            case(4):
-                division(num1, num2);
-                break;
+            switch (operation) {
+                case (1):
+                    addition();
+                    break;
+                case (2):
+                    subraction();
+                    break;
+                case (3):
+                    multiplication();
+                    break;
+                case (4):
+                    division();
+                    break;
+                default:
+                    System.out.println("Invalid choice!!!");
+            }
         }
     }
 }
